fd88c8e41fbd5377fa5e140330d1047130772130
Lorenzo Taylor <57335062+LorenzoTaylor@users.noreply.github.com>
2024-08-16 22:27:50 -0700

    Update AppBanner.jsx

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -49,9 +49,9 @@ const AppBanner = () => {
 					}}
 					className="flex justify-center sm:block"
 				>
-					<a
+					{/*<a
 						download="LorenzoTaylor-Resume.pdf"
-						href="/files/LorenzoTaylor-Resume.pdf"
+						href="/LorenzoTaylor-Resume.pdf"
 						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
 						aria-label="Download Resume"
 					>
@@ -59,7 +59,7 @@ const AppBanner = () => {
 						<span className="text-sm sm:text-lg font-general-medium duration-100">
 							Resume
 						</span>
-					</a>
+					</a>*/} 
 				</motion.div>
 			</div>
 			<motion.div
